57dca522fa2d0c62246967c022c829206aebeca1
amanjit-gill-data <amanjitgilldata@gmail.com>
2020-08-14 15:20:06 +1000

    Edit bash script.

--- daily_update.sh
+++ daily_update.sh
@@ -6,7 +6,7 @@ conda activate ag_python
 
 # navigate to python scripts and execute
 cd ~/Google\ Drive/Data\ Science/Projects/covid19_vic/scripts
-# python daily_cases.py
+python daily_cases.py
 python daily_images.py
 
 # navigate to project root as upcoming git commands need to be executed in repo root
@@ -18,5 +18,5 @@ ffmpeg -y -r:v 2.5 -i "data/images/melb/cases_absolute/%03d.png" -c:v libx264 -p
 
 # stage, commit and push changes
 git add .
-git commit -m "Test scheduled task on sleep."
+git commit -m "Perform auto-update of daily cases."
 git push
