b57a99b039e553a314dbcf3b8823a94cb7f5d096
amanjit-gill-data <amanjitgilldata@gmail.com>
2020-09-05 23:43:13 +1000

    Sandbox dash callbacks.

--- scripts/sandbox4.py
+++ scripts/sandbox4.py
@@ -2,25 +2,49 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     html.H6("Change value to see callbacks in action"),
-    html.Div(["Input: ", dcc.Input(id = 'my-input', value = 'initial value', type = 'text')]),
+    html.Div(["Input: ", dcc.Input(id = 'text-input', value = 'initial value', type = 'text')]),
     html.Br(),
-    html.Div(id = 'my-output')
+    html.Div(id = 'text-output'),
+
+    dcc.Graph(id = 'graph-with-slider'),
+    dcc.Slider(
+        id = 'year-slider', 
+        min = df['year'].min(),
+        max = df['year'].max(),
+        value = df['year'].min(),
+        marks = {str(i): str(i) for i in df['year'].unique()},
+        step = None
+    )
 ])
 
 @app.callback(
-    Output(component_id = 'my-output', component_property = 'children'),
-    [Input(component_id = 'my-input', component_property = 'value')]
+    [Output('text-output', 'children'),
+    Output('graph-with-slider', 'figure')],
+    [Input('text-input', 'value'),
+    Input('year-slider', 'value')]
 )
+def update_output_div_and_graph(input_text, input_year):
 
-def update_output_div(input_value):
-    return 'Output: {}'.format(input_value)
+    filtered_df = df[df.year == input_year]
+
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp", 
+                     size="pop", color="continent", hover_name="country", 
+                     log_x=True, size_max=55)
+
+    fig.update_layout(transition_duration=500)
+
+    return 'Output: {}'.format(input_text), fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
