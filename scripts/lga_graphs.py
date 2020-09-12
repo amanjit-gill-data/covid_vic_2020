2ff264cf80e6aa3ac1f2fef7dee3c62d93f35a9f
amanjit-gill-data <amanjitgilldata@gmail.com>
2020-09-13 00:10:12 +1000

    Create dash app for daily case graphs.

--- scripts/lga_graphs.py
+++ scripts/lga_graphs.py
@@ -2,8 +2,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
+import math
 
 df = pd.read_csv('../data/all_dates.csv')
 df.set_index('lga', inplace = True)
@@ -17,7 +18,10 @@ lga_keys = df.index.to_list()
 lga_dicts = []
 
 for lga in lga_keys:
-    lga_dicts.append(dict(zip(['label', 'value'], [lga, lga])))
+    lga_dicts.append(dict(zip(['label', 'value'], [lga, lga]))) # keys and values are the same
+
+# set layout of app
+# heading, then multi-select dropdown, then graph
 
 app.layout = html.Div([
 
@@ -33,36 +37,46 @@ app.layout = html.Div([
     dcc.Graph(id = 'out-graph')
 ])
 
+# set up callback
+# when the dropdown 'lga-input' is updated, the graph 'out-graph' will be updated
+
 @app.callback(
     Output('out-graph', 'figure'),
     [Input('lga-input', 'value')]
 )
 
+# this function is called whenever the dropdown is updated
+# if there is at least one LGA selected, the row for each LGA is extracted from the df (as a series)
+# and put into a list of series, then each series is plotted onto the figure
+# if there are no LGAs selected, an empty graph is returned
 def create_graph(lga_names):
+
+    fig = go.Figure()
 
     if (len(lga_names) > 0):
         series_list = [df.loc[lga] for lga in lga_names]
+        for series in series_list:
+            fig.add_trace(go.Scatter(
+                x = df.columns, 
+                y = series, 
+                mode = 'lines', 
+                name = series.name
+            ))
         max_yval = max([series.max() for series in series_list])
-        fig = px.line(
-            x = df.columns,
-            y = series_list, 
-            range_y = [0, max_yval+10],
-            labels = {'x': 'Date', 'value': 'Active Cases'}
-        )
+        fig.update_yaxes(range = [0, math.ceil(max_yval/100)*100]) # round y-axis up to next 100
     else:
-        fig = px.line(
+        fig.add_trace(go.Scatter(
             x = df.columns,
-            y = [0]*len(df.columns),
-            range_y= [0, 10],
-            labels = {'x': 'Date', 'y': 'Active Cases'}
-        )
+            y = [0]*len(df.columns)
+        ))
+        fig.update_yaxes(range = [0, 10])
+
     fig.update_xaxes(tick0 = '2020-07-01', dtick = 'M1')
-    fig.update_layout(legend = dict(
-        yanchor = 'top',
-        y = 0.99,
-        xanchor = 'left',
-        x = 0.01
-    ))
+    fig.update_layout(
+        legend = dict(yanchor = 'top', y = 0.99, xanchor = 'left', x = 0.01),
+        xaxis_title = 'Date',
+        yaxis_title = 'Active Cases'
+    )
     return fig
 
 if __name__ == '__main__':
